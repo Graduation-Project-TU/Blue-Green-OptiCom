86511ee027428e5dbd5fa040773f01b0a228b2b7
manal <manaldonia907@gmail.com>
2026-04-25 16:22:34 +0300

    Fix: Update Serial2 initialization to specify RX/TX pins to resolve communication issue between boards

--- ESP32_Receiver_Node.ino
+++ ESP32_Receiver_Node.ino
@@ -1,7 +1,7 @@
 void setup() {
   Serial.begin(115200);
   
-  Serial2.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
 
   Serial.println("Receiver ESP32 Waiting for data...");
 }
--- ESP32_Sensor_Node.ino
+++ ESP32_Sensor_Node.ino
@@ -11,8 +11,8 @@ DallasTemperature sensors(&oneWire);
 void setup() {
   Serial.begin(115200);
   
-  Serial2.begin(9600); 
-
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
+  
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   sensors.begin();
